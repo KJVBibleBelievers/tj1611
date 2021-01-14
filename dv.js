2a11192679b462cd7cfba06befe81b952dbf65c7
tjcommers <thomascommers@outlook.com>
2021-01-14 22:50:29 +0100

    Update dv.js

--- dv.js
+++ dv.js
@@ -1,3 +1,3 @@
-<script type="text/javascript">
+
 document.write("<iframe src=\"http://www.kingjamesonline.org/inspiring-images-widget.php\" style=\"width: 344px; height: 344px; border: 0px solid #ffffff;\"></iframe>");
-</script>
+
